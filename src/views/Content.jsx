e82e10b0e235ac2ca0c95fcbd37080af54ec6fe3
martinlunde <martlun@stud.ntnu.no>
2017-09-27 16:16:31 +0200

    Made todoList get rendered by first button

--- src/views/Content.jsx
+++ src/views/Content.jsx
@@ -4,12 +4,13 @@
 import React from 'react';
 import '../assets/styles/Content.css';
 import {Route} from 'react-router-dom';
+import {TodoList} from "./TodoList";
 
 export class Content extends React.Component {
     render(){
         return (
             <div className="content">
-                <Route path="/1" render={ () => <h1>WebApp1</h1>}/>
+                <Route path="/1" render={ () => <TodoList/>}/>
                 <Route path="/2" render={ () => <h1>WebApp2</h1>}/>
                 <Route path="/3" render={ () => <h1>WebApp3</h1>}/>
             </div>
