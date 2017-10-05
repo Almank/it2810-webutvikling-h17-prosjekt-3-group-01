4caf39087e468eca36fa3c338d173be5b56d22bc
Thay <thayanan@gmail.com>
2017-10-05 16:01:03 +0200

    Add hompage, styling to sidebar

---
+++ src/views/Frontpage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import '../assets/styles/Frontpage.css'
+
+
+
+export class Frontpage extends React.Component {
+    render(){
+        return (
+            <div>
+                <div className="hero">
+                    <div className="background-image"></div>
+                </div>
+                <div>
+                </div>
+            </div>
+        );
+    }
+}
--- src/views/SideBar.jsx
+++ src/views/SideBar.jsx
@@ -5,13 +5,14 @@ import React from 'react';
 import '../assets/styles/SideBar.css';
 import {Button} from "./components/SideBar/Button";
 
+
 export class SideBar extends React.Component {
     render(){
         return (
             <div className="SideBar">
-                <Button link="/1"/>
-                <Button link="/notebook"/>
-                <Button link="/calendar"/>
+                <Button link="/1" icon="glyphicon glyphicon-list-alt"/>
+                <Button link="/notebook" icon="glyphicon glyphicon-pencil"/>
+                <Button link="/calendar" icon="glyphicon glyphicon-calendar"/>
             </div>
         );
     }
