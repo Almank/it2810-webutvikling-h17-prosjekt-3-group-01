fae6062c178c1be6b4512d836d22ed8376da9580
Christoffer Almankaas <chrisa.a87@gmail.com>
2017-10-07 17:16:42 +0200

    Revamped styles on nsidebar, removing style rules that is not used.

--- src/views/SideBar.jsx
+++ src/views/SideBar.jsx
@@ -9,7 +9,7 @@ import {Button} from "./components/SideBar/Button";
 export class SideBar extends React.Component {
     render(){
         return (
-            <div className="SideBar">
+            <div className="sideBar">
                 <Button link="/" icon="glyphicon-home"/>
                 <Button link="/1" icon="glyphicon-list-alt"/>
                 <Button link="/notebook" icon="glyphicon-pencil"/>
