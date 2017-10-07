e1752b65ace58e22123fa353e870eb0beb1b8290
Christoffer Almankaas <chrisa.a87@gmail.com>
2017-10-07 17:58:50 +0200

    Added state and divs ready for content depending on what date you are at.

--- src/views/Calendar.jsx
+++ src/views/Calendar.jsx
@@ -6,6 +6,36 @@ import DayBox from './components/calendar/DayBox';
 import '../assets/styles/calendar.css';
 
 export class Calendar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {empty: true};
+    }
+
+    emptyScheduleCheck(){
+        switch (this.state.empty){
+            default:
+                break;
+            case true:
+                return (
+                    <div className='calendarBoxContent'>
+                        <h1>You don't have any plans!</h1>
+                    </div>
+                );
+            case false:
+                return (
+                    <div className='calendarBoxContent'>
+                        <div className='leftCalendarBoxContent'>
+                            <h2>Time</h2>
+                        </div>
+
+                        <div className='rightCalendarBoxContent'>
+                            <h1>Content</h1>
+                            <h2>Tinderdate at Kaffebrenneriet.</h2>
+                        </div>
+                    </div>
+                );
+        }
+    }
 
     render(){
         return (
@@ -15,8 +45,10 @@ export class Calendar extends React.Component {
                 <DayBox />
 
                 <div className={'calendarBox'}>
-
+                    {this.emptyScheduleCheck()}
                 </div>
+
+
             </div>
         );
     }
