bb5ee16fb7f5d7b068c75e4b4c8397bfeddd109f
martinlunde <martlun@stud.ntnu.no>
2017-10-12 17:11:57 +0200

    actually working navbar

---
+++ app/src/containers/Calendar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export class Calendar extends React.Component {
+    render(){
+        return(
+            <Text>Calendar</Text>
+        )
+    }
+}
--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import {SideBar} from "./SideBar";
+import { View, StyleSheet, Image } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import {Calendar} from "./containers/Calendar";
+import {Notes} from './containers/Notes';
+import {Todo} from './containers/Todo';
 
 export class Index extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <SideBar/>
-                <View/>
+                <Content/>
             </View>
         );
     }
@@ -19,5 +21,55 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'flex-end',
         flex: 1,
+    }
+});
+
+export const Content = TabNavigator({
+    Calendar: {screen: Calendar,
+            navigationOptions: {
+                tabBarIcon: () => (
+                    <Image
+                        source={require('./assets/calendar.png')}
+                        style={{width: 60, height: 60}}
+                    />
+                ),
+            }
+        },
+    Notes: {screen: Notes,
+            navigationOptions: {
+                tabBarIcon: () => (
+                    <Image
+                        source={require('./assets/list-symbol.png')}
+                        style={{width: 60, height: 60}}
+                    />
+                ),
+            }
+        },
+    Todo: {screen: Todo,
+            navigationOptions: {
+                tabBarIcon: () => (
+                    <Image
+                        source={require('./assets/pencil.png')}
+                        style={{width: 60, height: 60}}
+                    />
+                ),
+            }
+        }
+}, {
+    initialRouteName: 'Calendar',
+    swipeEnabled: false,
+    tabBarOptions: {
+
+        showIcon: true,
+        showLabel: false,
+        inactiveBackgroundColor: '#8e8e8e',
+        activeBackgroundColor: '#6e6e6e',
+        style: {
+            height: 100,
+        },
+        iconStyle: {
+            height: 50,
+            width: 50
+        },
     }
 });
