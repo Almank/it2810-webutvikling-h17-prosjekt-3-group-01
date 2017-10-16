c10110e8dd1ef28ca2228da5d810f2af15ac8f09
martinlunde <martlun@stud.ntnu.no>
2017-10-16 11:10:02 +0200

    failed to load files

--- app/src/containers/Calendar.js
+++ app/src/containers/Calendar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, AsyncStorage, Text, ScrollView } from 'react-native';
-import {Week} from '../components/Calendar/Week';
-import {AppointmentForm} from "../components/Calendar/AppointmentForm";
+import {Week} from '../components/calendar/Week';
+import {AppointmentForm} from "../components/calendar/AppointmentForm";
 
 export class Calendar extends React.Component {
     constructor(props) {
@@ -24,6 +24,7 @@ export class Calendar extends React.Component {
 
         this.loadData();
     }
+
 
     emptyScheduleCheck(){
 
