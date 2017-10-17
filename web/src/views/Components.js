105a017c742941ad60454fddf4aceb13a11aca5c
martinlunde <martlun@stud.ntnu.no>
2017-10-17 12:38:11 +0200

    Added Component.js to manage exports

---
+++ web/src/views/Components.js
@@ -0,0 +1,24 @@
+/* Exports all components such that imports in other files will be
+*  cleaner and less messy. */
+
+// Calendar components
+export {AppointmentForm} from './components/calendar/AppointmentForm';
+export {ContentHeader} from './components/calendar/ContentHeader';
+export {DayEle} from './components/calendar/DayEle';
+export {WeekCal} from './components/calendar/WeekCal';
+
+// Note components
+export {NoteContent} from './components/NoteList/NoteContent';
+export {NoteLink} from './components/NoteList/NoteLink';
+export {NoteList} from './components/NoteList/NoteList';
+
+// Sidebar components
+export {Button} from './components/SideBar/Button';
+
+// Todo components
+export {TodoCatelog} from './components/TodoList/TodoCatelog'
+export {TodoCatelogForm} from './components/TodoList/TodoCatelogForm'
+export {TodoFilter} from './components/TodoList/TodoFilter'
+export {TodoForm} from './components/TodoList/TodoForm'
+export {TodoList} from './components/TodoList/TodoList'
+export {ToDoListItem} from './components/TodoList/TodoListItem'
