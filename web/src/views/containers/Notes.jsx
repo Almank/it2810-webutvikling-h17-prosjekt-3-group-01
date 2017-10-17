b4886a23b633c55c68484f7d0642d2ff5d83af0f
martinlunde <martlun@stud.ntnu.no>
2017-10-17 13:19:56 +0200

    change note content css to be responsive

--- web/src/views/containers/Notes.jsx
+++ web/src/views/containers/Notes.jsx
@@ -58,24 +58,25 @@ export class Notes extends React.Component {
 
     render(){
         return (
-            <div> <h1 className={'title'}>Notes</h1>
-            <div className="NoteBook">
-                <NoteList notes={this.state.notes}
-                          removeClick={(value) => this.removeClick(value)}
-                          updateStorage={() => this.updateLocalStorage()}
-                />
-                <div className="NoteEdit">
-                    <div className="NoteEditContent">
-                        <Route exact path="/notebook/:name" render={ (id) =>
-                            <NoteContent data={this.state.notes[id.match.params.name]}
-                                         key={id}
-                                         id={id}
-                                         onChange={this.onChange.bind(this)}
-                                         update={this.state.update}
-                            />}/>
+            <div className="NoteBookWrapper">
+                <h1 className={'title'}> Notes</h1>
+                <div className="NoteBook">
+                    <NoteList notes={this.state.notes}
+                              removeClick={(value) => this.removeClick(value)}
+                              updateStorage={() => this.updateLocalStorage()}
+                    />
+                    <div className="NoteEdit">
+                        <div className="NoteEditContent">
+                            <Route exact path="/notebook/:name" render={ (id) =>
+                                <NoteContent data={this.state.notes[id.match.params.name]}
+                                             key={id}
+                                             id={id}
+                                             onChange={this.onChange.bind(this)}
+                                             update={this.state.update}
+                                />}/>
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         );
     }
