44a78a1cec2382018558606096825d65d04181d2
Christoffer Almankaas <chrisa.a87@gmail.com>
2017-10-17 19:49:01 +0200

    Set validation on setting appointments. You can only do that for the coming week.

--- web/src/views/containers/Calendar.jsx
+++ web/src/views/containers/Calendar.jsx
@@ -32,7 +32,6 @@ export class Calendar extends React.Component {
         this.createAppointment = this.createAppointment.bind(this);
         this.changeContent = this.changeContent.bind(this);
         this.handleRemoveClick = this.handleRemoveClick.bind(this);
-        this.updateLocalStorage = this.handleRemoveClick.bind(this);
         this.handleChangeClick = this.handleChangeClick.bind(this);
     }
 
@@ -178,7 +177,9 @@ export class Calendar extends React.Component {
     }
 
     validateFormDate(date){
-        return date.length === 10 && date >= new Date().toISOString().slice(0, 10);
+        let lastDate = new Date();
+        lastDate.setDate(lastDate.getDate() + 7);
+        return date.length === 10 && date >= new Date().toISOString().slice(0, 10) && date < lastDate.toISOString().slice(0, 10);
     }
 
     validateFormTime(time){
